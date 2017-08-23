eb05bf9026bcd8818029f336946d6902bafd6f66
Vincent Jurcisin-Kukla <vjurcisin@gmail.com>
2017-08-23 21:22:42 +0200

    added spring boot
    Signed-off-by: Vincent Jurcisin-Kukla <vjurcisin@gmail.com>

---
+++ photostore/src/main/java/sk/kukla/jurcisin/vincent/photostore/PhotostoreApplication.java
@@ -0,0 +1,12 @@
+package sk.kukla.jurcisin.vincent.photostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotostoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotostoreApplication.class, args);
+	}
+}
